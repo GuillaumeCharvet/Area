5953d4d0b448a050638d86de476a045b5694bb73
Guillaume Charvet <guicharvet@orange.fr>
2021-06-22 17:11:54 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,9 +1,13 @@
-var main_poly;
-var graphics;
+const etapes_convexify = 50;
 
+var graphics;
+var main_poly;
+
+var random_poly = [];
+var random_poly_size = 40;
 var timer = 0;
 
-const etapes_convexify = 50;
+var relaxation_time = 300;
 
 class menu extends Phaser.Scene{
     
@@ -25,18 +29,27 @@ class menu extends Phaser.Scene{
         graphics = this.add.graphics();
 
         //main_poly = [[200,200],[250,250],[300,200],[300,300],[200,300]];
-        main_poly = [[100,100],[300,200],[500,100],[400,300],[500,500],[300,400],[100,500],[200,300]];
+        main_poly = [[100,100],[300,200],[500,100],[400,300],[500,500],[300,400],[100,500],[200,300],];
 
-        convexify(main_poly);
+        for (let index = 0; index < random_poly_size; index++)
+        {
+            let theta = 2*Math.PI*index/random_poly_size;
+            let radius = Math.floor(Math.random()*180);
+            //random_poly.push([Math.round(Math.random()*config.width),Math.round(Math.random()*config.height)]);
+            random_poly.push([config.width/2+(50+radius)*Math.cos(theta),config.height/2+(50+radius)*Math.sin(theta)]);
+        }       
 
         //setTimeout(()=>{graphics.clear();draw_tabVertex(main_poly);},1000);
     }
     
     update ()
     {
+        timer++;
+        if(timer%(Math.round(60*relaxation_time/1000))==0){convexify(random_poly);}
 
         graphics.clear();
-        draw_tabVertex(main_poly);
+        //draw_tabVertex(main_poly);
+        draw_tabVertex(random_poly);
     }
 }
 
@@ -74,13 +87,13 @@ function convexify (tab){
                 setTimeout(()=>{
                     tab[j][0] = (1-alpha)*tab_aux[j][0]+alpha*(tab_aux[i][0]+tab_aux[k][0])/2;
                     tab[j][1] = (1-alpha)*tab_aux[j][1]+alpha*(tab_aux[i][1]+tab_aux[k][1])/2;
-                },index/etapes_convexify*1000);
+                },index/etapes_convexify*relaxation_time);
             }
         }        
     }
 }
 
-function xsin(x){return x+1.2*Math.sin(2*Math.PI*Math.pow(x,2)*4)*Math.cos(2*Math.PI*x/4)}
+function xsin(x){return x+1.2*Math.sin(2*Math.PI*Math.pow(x,2))*Math.cos(2*Math.PI*x/4)}
 /*
 function xsin(xi){
     return Math.pow(xi,1/4)*(4+(Math.sin(2*2*Math.PI*xi))*Math.cos(2*Math.PI*xi/4))/4;
